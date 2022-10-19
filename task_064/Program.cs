0657efc556938fb4c8acf5dc31b220eeeaadf0e4
goodie <vorkuta@inbox.ru>
2022-10-20 00:53:46 +0300

    task_064 completed

---
+++ task_064/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Write("Введите число N: ");
+        int n = int.Parse(ReadLine()!);
+
+        if (n < 1) throw new Exception("Ожидается N > 0");
+
+        WriteLine(NumToString(n));
+
+        static string NumToString(int n)
+        {
+            return n > 1 ? $"{n},{NumToString(n - 1)}" : $"{n}";
+        }
+    }
+}
